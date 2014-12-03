cc577df9deebf7bf1ae591932b668f99fe78108c
Sahaj Biyani <sahaj@Sahajs-MacBook-Pro.local>
2014-12-03 03:45:31 -0800

    read write block

--- layer1.c
+++ layer1.c
@@ -16,12 +16,20 @@
 int read_block(void* buffer, type block_num, type offset, type size){
     if(offset >= BLOCK_SIZE){
 	printf("offset out of bound");
-	return 0;
+	return -1;
     }
     
-    get_block(buffer, block_num);
+    if(offset+size > BLOCK_SIZE){
+	printf("some data might not be read. read size > block size.\n");
+	size = BLOCK_SIZE - offset;
+    }
 
-    
+    // get whole block then extract offset
+    char read_buffer[BLOCK_SIZE]; 
+    get_block(read_buffer, block_num);
+    memcpy(buffer, read_buffer+offset, size);
+
+    return size;
 }
 
 bool read_inode(type inode_num, struct node* inodep){
@@ -30,12 +38,31 @@ bool read_inode(type inode_num, struct node* inodep){
 	return 0;
     }
     type block_num = (inode_num-1)*(INODE_SIZE)/(BLOCK_SIZE) + 1;
-    type offset = (inode_num-1)*(INODE_SIZE)%(BLOCK_SIZE); //Inode has to be multiple of block size
+    type offset = (inode_num-1)*(INODE_SIZE)%(BLOCK_SIZE); //inode has to be multiple of block size
     read_block(nodep, block_num,offset,sizeof(struct inode));
+    return 1;
 }
 
-    
-    read_block(
+bool write_block(const void* buffer, type block_num, type offset, type size){
+    if(offset >= BLOCK_SIZE){
+	printf("offset out of bound");
+	return 0;
+    }
+
+    if(offset+size > BLOCK_SIZE){
+	printf("Can not write all data. data size > block size.\n");
+	return 0;
+    }
+
+    char read_buffer[BLOCK_SIZE];
+    get_block(read_buffer, block_num);
+    memcpy(read_buffer+offset, buffer, size);
+
+    put_block(buffer, block_num);
+
+    return 1;
+}
+
 
 
 
